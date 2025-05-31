46e918ce2dfb6a9de9d1eafc270994ec51b23762
doaamohamed88 <doaatawfik82001@gmail.com>
2025-05-31 22:23:35 +0300

    initial leader board page

---
+++ Frontend/src/pages/LeaderBoard/LeaderBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './LeaderBoard.module.scss'
+
+export default function leaderboard() {
+  return (
+    <div className={styles.leaderboard}>leaderboard</div>
+  )
+}
