d74efdd44235b34888820251ba27fe6bd62a1f97
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2025-06-01 17:38:15 +0300

    routing

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import classes from './Header.module.css';
-import i18n from '../../i18n';
-import * as FaIcons from 'react-icons/fa6'
+import React, { useState, useEffect } from "react";
+import classes from "./Header.module.css";
+import i18n from "../../i18n";
+import * as FaIcons from "react-icons/fa6";
 function Header() {
-    const [currentLang, setCurrentLang] = useState(i18n.language);
+  const [currentLang, setCurrentLang] = useState(i18n.language);
 
-    const handleLanguageChange = (lang) => {
-        i18n.changeLanguage(lang)
-            .then(() => setCurrentLang(lang))
-            .catch((err) => console.error("Error changing language:", err));
-    };
+  const handleLanguageChange = (lang) => {
+    i18n
+      .changeLanguage(lang)
+      .then(() => setCurrentLang(lang))
+      .catch((err) => console.error("Error changing language:", err));
+  };
 
-    useEffect(() => {
-        document.documentElement.dir = currentLang === 'ar' ? 'rtl' : 'ltr';
-    }, [currentLang]);
+  useEffect(() => {
+    document.documentElement.dir = currentLang === "ar" ? "rtl" : "ltr";
+  }, [currentLang]);
 
-    return (
-        <div className={classes.header}>
-            <button onClick={() => handleLanguageChange(currentLang === 'ar' ? 'en' : 'ar')}>
-                {currentLang === 'ar' ? 'en' : 'ar'}
-            </button>
-            <button>
-                <FaIcons.FaLightbulb />
-            </button>
-        </div>
-    );
+  return (
+    <div className={classes.header}>
+      <button
+        onClick={() => handleLanguageChange(currentLang === "ar" ? "en" : "ar")}
+      >
+        {currentLang === "ar" ? "en" : "ar"}
+      </button>
+      <button>
+        <FaIcons.FaLightbulb />
+      </button>
+    </div>
+  );
 }
 
 export default Header;
--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -3,11 +3,41 @@ import AuthenticationPage from "../pages/AuthenticationPage/AuthenticationPage";
 import LeaderBoard from "../pages/leaderboard/leaderboard";
 import BoardPage from "../pages/BoardPage/BoardPage";
 import AdminDashboard from "../pages/adminDashboard/AdminDashboard";
+import MainLayout from "../layouts/MainLayout";
+import LandingPage from "../Pages/Landing/Landing";
+import BoardLayout from "../layouts/BoardLayout";
+
+const loggedIn = true;
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <AuthenticationPage type={"login"} />,
+    element: loggedIn ? <MainLayout /> : <AuthenticationPage type={"login"} />,
+    children: loggedIn
+      ? [
+          {
+            path: "/",
+            element: <LandingPage />,
+          },
+          {
+            element: <BoardLayout />,
+            children: [
+              {
+                path: "/tasks",
+                element: <BoardPage />,
+              },
+              {
+                path: "/leader-board",
+                element: <LeaderBoard />,
+              },
+              {
+                path: "/dashboard",
+                element: <AdminDashboard />,
+              },
+            ],
+          },
+        ]
+      : null,
   },
   {
     path: "/register",
@@ -17,16 +47,4 @@ export const routes = createBrowserRouter([
     path: "/forget-password",
     element: <AuthenticationPage type={"forgetPassword"} />,
   },
-  {
-    path: "/leader-board",
-    element: <LeaderBoard />,
-  },
-  {
-    path: "/board",
-    element: <BoardPage />,
-  },
-  {
-    path: "/dashboard",
-    element: <AdminDashboard></AdminDashboard>,
-  }
 ]);
