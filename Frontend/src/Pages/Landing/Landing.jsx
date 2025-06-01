1c7e9f9d2633e00eefe167c42e94b05b91687951
Jameeelaahmed <gamilas320@gmail.com>
2025-06-01 16:38:09 +0300

    update package.json & lock

--- Frontend/src/Pages/Landing/Landing.jsx
+++ Frontend/src/Pages/Landing/Landing.jsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from 'react';
-import Card from '../../Components/Card/Card';
+import Card from '../../components/Card/Card';
 import classes from './Landing.module.css';
 
 export default function LandingPage() {
-    // const fullText = "Welcome to your personal task space.";
-    // const [text, setText] = useState('');
 
-    // useEffect(() => {
-    //     let index = 0;
-
-    //     const interval = setInterval(() => {
-    //         setText(prev => {
-    //             const nextText = fullText.slice(0, index + 1);
-    //             index++;
-
-    //             if (index >= fullText.length) {
-    //                 clearInterval(interval);
-    //             }
-
-    //             return nextText;
-    //         });
-    //     }, 80);
-
-    //     return () => clearInterval(interval);
-    // }, [fullText]);
 
     return (
         <div className={classes.landing_page}>
