884ae58f4d1e9f222eb177d011bf1ab680665ca7
Jameeelaahmed <gamilas320@gmail.com>
2025-06-01 14:11:49 +0300

    update

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import AuthenticationPage from "../pages/AuthenticationPage/AuthenticationPage";
 import BoardPage from "../pages/BoardPage/BoardPage";
+import RootLayout from "./RootLayout";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <AuthenticationPage type={"login"} />,
+    // element: <AuthenticationPage type={"login"} />,
+    element: <RootLayout />
   },
   {
     path: "/register",
