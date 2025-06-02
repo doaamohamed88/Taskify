2d96ce24b6496d9660ca0c39e6342af0a55c8ba1
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-06-02 09:05:59 +0300

    feat(adminDashboard, sidebar localization): apply localization to ar, and handling rtl styles

--- Frontend/src/Components/Sidebar/SideBarButton/SideBarButton.jsx
+++ Frontend/src/Components/Sidebar/SideBarButton/SideBarButton.jsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import st from '../sidebar.module.css';
 import { useLocation, useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 export default function SideBarButton({ title, icon, active, linkTo }) {
+
+  const { t } = useTranslation()
 
   const navigate = useNavigate()
   const location = useLocation()
@@ -16,7 +19,7 @@ export default function SideBarButton({ title, icon, active, linkTo }) {
   return (
     <div className={`${st.sidebarButton} ${active ? st.active : ''}`} onClick={() => changePage(linkTo)}>
         <FontAwesomeIcon icon={icon} className={st.sidebarButtonIcon}></FontAwesomeIcon>
-        <span className={st.desktopOnly}>{title}</span>
+        <span className={st.desktopOnly}>{t(`${title}`)}</span>
     </div>
   )
 }
