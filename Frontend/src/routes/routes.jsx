0c31e55bafbea877d718bc95264f21a4cfd3de53
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-06-03 09:45:36 +0300

    loggedIn variable original value

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -9,8 +9,8 @@ import BoardLayout from "../layouts/BoardLayout";
 import AdminDashboard from "../Pages/AdminDashboard/AdminDashboard";
 
 const AppRoutes = () => {
-  // const loggedIn = useSelector((state) => state.user);
-  const loggedIn = true
+  const loggedIn = useSelector((state) => state.user);
+  // const loggedIn = true
 
   const routes = createBrowserRouter([
     {
