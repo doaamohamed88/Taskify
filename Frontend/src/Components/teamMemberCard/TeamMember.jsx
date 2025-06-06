bda62f3d4d3726b3da65bfdcc3b3a13db346595a
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-06-06 18:20:00 +0300

    style(delete member modal): change delete member modal style and use modal component

--- Frontend/src/Components/teamMemberCard/TeamMember.jsx
+++ Frontend/src/Components/teamMemberCard/TeamMember.jsx
@@ -1,36 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import st from "./teamMember.module.css"
 import { faTrash, faUser } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
 import { useTranslation } from "react-i18next"
+import Modal from "../Modal/Modal"
+import { useRef } from "react"
 
 export default function TeamMember({ name }) {
-  const [showModal, setShowModal] = useState(false)
   const { t } = useTranslation()
+  const modalRef = useRef()
 
   return (
-    <div className={st.memberCard}>
-      <div className={st.left}>
-        <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-        {name}
-      </div>
-      <div>
-        <FontAwesomeIcon
-          icon={faTrash}
-          className={st.deleteIcon}
-          onClick={() => setShowModal(true)}
-        ></FontAwesomeIcon>
+    <>
+      <div className={st.memberCard}>
+        <div className={st.left}>
+          <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+          {name}
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faTrash}
+            className={st.deleteIcon}
+            onClick={() => modalRef.current.open()}
+          ></FontAwesomeIcon>
+        </div>
       </div>
 
-      {showModal && (
-        <div className={st.deleteModal}>
-          <div className={st.modal}>
-            <p>{t("deleteMemberConfirm", { name })}</p>
-            <button onClick={() => setShowModal(false)}>{t("Cancel")}</button>
+      <Modal ref={modalRef}>
+        <div className={st.modal}>
+          <p>{t("deleteMemberConfirm", { name })}</p>
+          <div className="buttons">
+            <button onClick={() => modalRef.current.close()}>{t("Cancel")}</button>
             <button>{t("Yes, Delete")}</button>
           </div>
         </div>
-      )}
-    </div>
+      </Modal>
+    </>
   )
 }
