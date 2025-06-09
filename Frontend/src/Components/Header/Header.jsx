280d06deb0e2884711f7ae51433bd7f797283ab1
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-06-09 15:24:53 +0300

    feat(header icons): toggle theme icon, add sign out button

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,41 +1,59 @@
-import { useState, useEffect } from "react";
-import classes from "./Header.module.css";
-import i18n from "../../i18n";
-import * as FaIcons from "react-icons/fa6";
-import { Link } from 'react-router-dom'
-import { useTheme } from "../../context/ThemeContext";
+import { useState, useEffect } from "react"
+import classes from "./Header.module.css"
+import i18n from "../../i18n"
+import * as FaIcons from "react-icons/fa6"
+import { Link } from "react-router-dom"
+import { useTheme } from "../../context/ThemeContext"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
 function Header() {
-const [currentLang, setCurrentLang] = useState(i18n.language);
-const { toggleTheme } = useTheme();
+  const [currentLang, setCurrentLang] = useState(i18n.language)
+  const [themeIcon, setThemeIcon] = useState("light")
+  const { toggleTheme } = useTheme()
 
   const handleLanguageChange = (lang) => {
     i18n
       .changeLanguage(lang)
       .then(() => setCurrentLang(lang))
-      .catch((err) => console.error("Error changing language:", err));
-  };
+      .catch((err) => console.error("Error changing language:", err))
+  }
 
   useEffect(() => {
-    document.documentElement.dir = currentLang === "ar" ? "rtl" : "ltr";
-  }, [currentLang]);
+    document.documentElement.dir = currentLang === "ar" ? "rtl" : "ltr"
+  }, [currentLang])
 
   return (
     <div className={classes.header}>
-      <Link to='/'>
-        <p className={classes.logo}>Taskify</p>
-      </Link>
       <div>
         <button
-          onClick={() => handleLanguageChange(currentLang === "ar" ? "en" : "ar")}
+          onClick={() => {
+            toggleTheme()
+            setThemeIcon(themeIcon === "light" ? "dark" : "light")
+          }}
         >
+          {themeIcon === "light" ? (
+            <FontAwesomeIcon icon={faSun}></FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
+          )}
+        </button>
+        <button onClick={() => handleLanguageChange(currentLang === "ar" ? "en" : "ar")}>
           {currentLang === "ar" ? "en" : "ar"}
         </button>
-        <button onClick={toggleTheme}>
-          <FaIcons.FaLightbulb />
+      </div>
+
+      <Link to="/">
+        <p className={classes.logo}>Taskify</p>
+      </Link>
+
+      <div>
+        <button className={classes.signOut}>
+          <FaIcons.FaUser />
+          Sign Out
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
